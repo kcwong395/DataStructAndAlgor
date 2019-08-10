bba39d92f4704d62eb4e7786cf5cebe0e88dc2b3
ElderHorse <kcwong395@gmail.com>
2019-08-10 16:59:54 +0800

    update Palindrome Linked List

---
+++ LeetCode/Easy/Palindrome Linked List/vector.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#include <vector>
+
+class Solution {
+public:
+	bool isPalindrome(ListNode* head) {
+		if (head == nullptr || head->next == nullptr) return true;
+		vector<int> v;
+		for (ListNode* p = head; p != nullptr; p = p->next) {
+			v.push_back(p->val);
+		}
+		for (int i = 0; i < v.size() / 2; i++) {
+			if (v[i] != v[v.size() - 1 - i]) return false;
+		}
+		return true;
+	}
+};
