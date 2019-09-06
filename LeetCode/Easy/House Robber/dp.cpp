bc4d338fa5fe0758ced42dd87d3bb082df50336c
ElderHorse <kcwong395@gmail.com>
2019-09-06 14:12:29 +0800

    update House Robber

---
+++ LeetCode/Easy/House Robber/dp.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	int rob(vector<int>& nums) {
+
+		if (nums.size() == 0) return 0;
+
+		int dp[nums.size() + 1] = { 0 };
+
+		dp[1] = nums[0];
+
+		for (int i = 2; i < nums.size() + 1; i++) {
+			dp[i] = max(dp[i - 2] + nums[i - 1], dp[i - 1]);
+		}
+
+		return dp[nums.size()];
+	}
+};
